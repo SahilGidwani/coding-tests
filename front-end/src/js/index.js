18780eced924b80b58f5ad35a0c8f889ab367579
SahilGidwani <sahilgidwani98@gmail.com>
2025-08-29 19:22:59 +0530

    Final changes for frontend task

--- front-end/src/js/index.js
+++ front-end/src/js/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const mobileMenu = document.getElementById('mobile-menu');
+const menuToggle = document.getElementById('menu-toggle');
+const body = document.body;
+
+/* Event listener for toggling the mobile menu */
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('open');
+  mobileMenu.classList.toggle('hidden');
+
+  if(menuToggle.classList.contains('open')) {
+    body.classList.add('overflow-hidden');
+  } else {
+    body.classList.remove('overflow-hidden');
+  }
+});
